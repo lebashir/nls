334fbf7ce9113feef8712216ca93a8a50ac25c15
lebashir <bashir.abihabib@gmail.com>
2021-05-06 12:04:59 -0500

    completed implementation of ls using Promise.all

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
+#!/usr/bin/env node
+
 const fs = require('fs');
+const chalk = require('chalk');
+const path = require('path');
+
+
+chalk.enabled = true;
+chalk.level = 3;
+
+const { lstat } = fs.promises;
+
+const targetDir = process.argv[2] || process.cwd()
+
+fs.readdir(targetDir, async (err, filenames) => {
+    if(err) {
+        console.log(err);
+    }
+    const statPromises = filenames.map(filename => {
+        return lstat(path.join(targetDir, filename));
+    });
+    const allStats = await Promise.all(statPromises);
+    for (let stats of allStats) {
+        const index = allStats.indexOf(stats);
+        if (stats.isFile()) {
+            console.log(filenames[index])
+        } else {
+            console.log(chalk.yellow(filenames[index]))
+        }
+        
+    }
+});
+
+//Method 1
+// const lstat = (filename) => {
+//     return new Promise((resolve, reject) => {
+//         fs.lstat(filename, (err, stats) => {
+//             if (err) {
+//                 reject(err);
+//             }
+//             resolve(stats);
+//         })
+//     });
+// };
